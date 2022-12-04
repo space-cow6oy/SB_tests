607e1eff6981b12052cb13fddf1bb5a980fd9fce
artöm <73524291+space-cow6oy@users.noreply.github.com>
2022-12-05 02:25:25 +0300

    Update SB_SQL_2.sql

--- SB_SQL_2.sql
+++ SB_SQL_2.sql
@@ -4,9 +4,9 @@ ROW_NUMBER() OVER(PARTITION BY name ORDER BY (SELECT NULL)) as rn
 from  people
 	cross apply string_split(name , ' ')
 )
-select [2] as Имя ,
-				[1] as Фамилия , 
-				[3] as Отчество
+select [2] as РРјСЏ ,
+				[1] as Р¤Р°РјРёР»РёСЏ , 
+				[3] as РћС‚С‡РµСЃС‚РІРѕ
 from c pivot(max(value) for rn in([2] , [1] , [3]  ) ) as pv
 
 
